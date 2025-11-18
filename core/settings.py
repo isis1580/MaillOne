b9c009714d54a01baa90beab7bc490117f8e1e91
Isis Cissé <zoomper2017@gmail.com>
2025-11-18 17:20:50 +0100

    message de reussite

--- core/settings.py
+++ core/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = "core.wsgi.application"
 #}
 
 DATABASES = {
-    "default": dj_database_url.parse(config('DATABASES_URL'))
+    "default": dj_database_url.parse(config('DATABASE_URL'))
 }
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
