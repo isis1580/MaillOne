7f8546221f2a57a68edd26bf1453eb8e649c580d
Isis Cissé <zoomper2017@gmail.com>
2025-11-28 13:48:50 +0100

    probleme avec style 1

--- core/settings.py
+++ core/settings.py
@@ -24,7 +24,7 @@ load_dotenv(BASE_DIR / '.env')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     'maillone-1.onrender.com',
